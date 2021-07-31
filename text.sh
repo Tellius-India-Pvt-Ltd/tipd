093f8d63dba7a3409eaabb216194bcc2c0039cfd
LokendraReddy426 <reddyglokendra426@gmail.com>
2021-07-31 06:05:57 +0000

    text file

---
+++ text.sh
@@ -0,0 +1,4 @@
+Dear All
+
+Shell script is a program, whatever commands are your typing or executing in the OS. It will give to OS to run the shell script.
+It is a combination of linux commands.if u know linux commands shell script is easy. 
