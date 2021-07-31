9723764496816f8a3fa292678627c21fdc57fc11
LokendraReddy426 <82992337+LokendraReddy426@users.noreply.github.com>
2021-07-31 11:58:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
 Variable is a memory location, it is going to store the value.
+There are two types of variables
+ 1. System defined variable 
+ 2. User defined variable.
 string is a set of characters. Git Hub is an hosted for Git.
